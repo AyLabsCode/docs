9afd1bc1e39cad754fe7c7788f84cd740f90b005
AymericLeFeyer <lefeyer.aymeric@gmail.com>
2025-11-30 16:26:59 +0100

    chore(app): setup github pages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,10 +14,10 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://AyLabsCode.docs.io',
+  url: 'https://aylabscode.docs.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/docs',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.

3372ba88c4634eced6e82ae39c6f56a6f9b4ac6b
Aymeric Le Feyer <aymeric.lefeyer.partner@decathlon.com>
2025-12-08 17:51:34 +0100

    fix(index): update link

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,7 +25,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/domotique/intro">
+            to="/domotique/intro">
             Démarrer la lecture 📖
           </Link>
         </div>
